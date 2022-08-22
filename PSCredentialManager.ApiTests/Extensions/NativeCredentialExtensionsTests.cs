b478923eacaddf2f2389da93bad75d2cd4bb47e0
Markus Szumovski <echalone@hotmail.com>
2022-08-22 12:50:25 +0200

    Added unittest for empty values

--- PSCredentialManager.ApiTests/Extensions/NativeCredentialExtensionsTests.cs
+++ PSCredentialManager.ApiTests/Extensions/NativeCredentialExtensionsTests.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PSCredentialManager.Api.Extensions;
 using PSCredentialManager.Common;
 using PSCredentialManager.Common.Enum;
+using System.Linq;
 
 namespace PSCredentialManager.ApiTests.Extensions
 {
@@ -50,6 +51,66 @@ namespace PSCredentialManager.ApiTests.Extensions
                 TargetName = "server01",
                 Type = CredType.Generic,
                 UserName = "test-user"
+            };
+
+            NativeCredential nativeCredential = credential.ToNativeCredential();
+
+            Assert.IsNotNull(nativeCredential);
+            Assert.IsInstanceOfType(nativeCredential, typeof(NativeCredential));
+            string ncUserName = System.Runtime.InteropServices.Marshal.PtrToStringUni(nativeCredential.UserName);
+            Assert.IsNotNull(ncUserName);
+            Assert.AreEqual(credential.UserName, ncUserName);
+
+            Credential convertedOnlyClear = nativeCredential.ToCredential(true, false);
+            Assert.IsNotNull(convertedOnlyClear);
+            Assert.IsInstanceOfType(convertedOnlyClear, typeof(Credential));
+            Assert.IsNotNull(convertedOnlyClear.UserName);
+            Assert.AreEqual(credential.UserName, convertedOnlyClear.UserName);
+            Assert.IsNotNull(convertedOnlyClear.Password);
+            Assert.AreEqual(credential.Password, convertedOnlyClear.Password);
+            Assert.AreEqual(credential.PaswordSize, convertedOnlyClear.PaswordSize);
+            Assert.IsNull(convertedOnlyClear.SecurePassword);
+
+            Credential convertedOnlySecure = nativeCredential.ToCredential(false, true);
+            Assert.IsNotNull(convertedOnlySecure);
+            Assert.IsInstanceOfType(convertedOnlySecure, typeof(Credential));
+            Assert.IsNotNull(convertedOnlySecure.UserName);
+            Assert.AreEqual(credential.UserName, convertedOnlySecure.UserName);
+            Assert.IsNotNull(convertedOnlySecure.SecurePassword);
+            Assert.AreEqual(credential.Password, convertedOnlySecure.SecurePassword.ToInsecureString());
+            Assert.AreEqual(uint.MinValue, convertedOnlySecure.PaswordSize);
+            Assert.IsNull(convertedOnlySecure.Password);
+
+            Credential convertedClearAndSecure = nativeCredential.ToCredential(true, true);
+            Assert.IsNotNull(convertedClearAndSecure);
+            Assert.IsInstanceOfType(convertedClearAndSecure, typeof(Credential));
+            Assert.IsNotNull(convertedClearAndSecure.UserName);
+            Assert.AreEqual(credential.UserName, convertedClearAndSecure.UserName);
+            Assert.IsNotNull(convertedClearAndSecure.Password);
+            Assert.IsNotNull(convertedClearAndSecure.SecurePassword);
+            Assert.AreEqual(credential.Password, convertedClearAndSecure.Password);
+            Assert.AreEqual(credential.PaswordSize, convertedClearAndSecure.PaswordSize);
+            Assert.AreEqual(credential.Password, convertedClearAndSecure.SecurePassword.ToInsecureString());
+            Assert.AreEqual(credential.PaswordSize, (uint)convertedClearAndSecure.SecurePassword.Length * 2);
+
+        }
+
+        [TestMethod()]
+        public void ToCredentialTestFilledEmptyValues()
+        {
+            Credential credential = new Credential()
+            {
+                AttributeCount = 0,
+                Attributes = new IntPtr(0),
+                Comment = "",
+                Password = "",
+                PaswordSize = 0,
+                Flags = 0,
+                LastWritten = DateTime.Now,
+                Persist = CredPersist.LocalMachine,
+                TargetName = "",
+                Type = CredType.Generic,
+                UserName = ""
             };
 
             NativeCredential nativeCredential = credential.ToNativeCredential();
